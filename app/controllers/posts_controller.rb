757f0aa8151a0d15678ee56a9f93a665907a067c
Erol Guzoğlu <erolguzoglu@gmail.com>
2015-05-15 03:03:00 +0300

    Add associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514225154) do
+ActiveRecord::Schema.define(version: 20150514235012) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150514225154) do
     t.datetime "deadline"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
